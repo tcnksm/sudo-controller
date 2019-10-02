68283e08756bd536ffd487e2087ee09ad1eb3c6e
tcnksm <nsd22843@gmail.com>
2019-10-02 16:48:36 +0900

    Add Kind TemporaryClusterRoleBinding

---
+++ api/v1/temporaryclusterrolebinding_types.go
@@ -0,0 +1,54 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:scope=Cluster
+
+// TemporaryClusterRoleBinding is the Schema for the temporaryclusterrolebindings API
+type TemporaryClusterRoleBinding struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// TTL is expiration time of temporary ClusterRoleBinding
+	// +optional
+	TTL metav1.Time `json:"ttl,omitempty"`
+
+	// Approver is an approver of ClusterRoleBinding
+	Approver string `json:"approver,omitempty"`
+
+	// The followings are same type which ClusterRoleBinding has
+	Subjects []rbacv1.Subject `json:"subjects,omitempty"`
+	RoleRef  rbacv1.RoleRef   `json:"roleRef,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// TemporaryClusterRoleBindingList contains a list of TemporaryClusterRoleBinding
+type TemporaryClusterRoleBindingList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []TemporaryClusterRoleBinding `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&TemporaryClusterRoleBinding{}, &TemporaryClusterRoleBindingList{})
+}
--- main.go
+++ main.go
@@ -19,6 +19,8 @@ import (
 	"flag"
 	"os"
 
+	sudocontrollerv1 "github.com/tcnksm/sudo-controller/api/v1"
+	"github.com/tcnksm/sudo-controller/controllers"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -35,6 +37,7 @@ var (
 func init() {
 	_ = clientgoscheme.AddToScheme(scheme)
 
+	_ = sudocontrollerv1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -59,6 +62,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.TemporaryClusterRoleBindingReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("TemporaryClusterRoleBinding"),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "TemporaryClusterRoleBinding")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
